ce5ad6847013f7841881066eb3695932b72b9133
Aline <angeline.fm@gmail.com>
2026-09-21 13:00:19 -0300

    refactor(textoff): remove patch degrade da Central

--- interface_web/processing_web.py
+++ interface_web/processing_web.py
@@ -1888,9 +1888,6 @@ def run_job(job,payload):
             elif job.action=="textoff_level3_preview":
                 from processamento.limpeza_baloes.textoff_level3_correction import generate_preview_job
                 job.result=generate_preview_job(manga,chs,payload)
-            elif job.action=="textoff_gradient_patch_preview":
-                from processamento.limpeza_baloes.textoff_gradient_patch import generate_gradient_preview_job
-                job.result=generate_gradient_preview_job(manga,chs,payload)
             elif job.action=="textoff_level3_approve":
                 from processamento.limpeza_baloes.textoff_level3_correction import approve_proposal_job
                 job.result=approve_proposal_job(manga,chs,payload)
